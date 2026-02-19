c976a33ca809a97f04f33cc5a4f920e9766053c9
zeriong <jaeryong95@gmail.com>
2026-02-19 22:08:18 +0900

    fix: 아코디언 스크롤 발생 시 레이아웃 밀림 현상 수정 (scrollbar-gutter: stable)

--- src/pages/__root.tsx
+++ src/pages/__root.tsx
@@ -6,7 +6,7 @@ const RootLayout = () => (
   <>
     <main className="flex h-screen w-full overflow-hidden bg-white">
       <SideBar />
-      <div className="flex justify-center grow overflow-auto">
+      <div className="flex justify-center grow overflow-auto [scrollbar-gutter:stable]">
         <div className="min-w-0 flex-1 pt-16 md:pt-0 max-w-[900px] flex justify-center grow">
           <Outlet />
         </div>
